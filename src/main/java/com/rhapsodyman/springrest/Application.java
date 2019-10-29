7d6184d1b995cf66e7dfc6f3afd191ca3b8c1f26
Andrei <malai281092@gmail.com>
2019-10-29 11:48:04 +0200

    Comments

--- src/main/java/com/rhapsodyman/springrest/Application.java
+++ src/main/java/com/rhapsodyman/springrest/Application.java
@@ -5,11 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
-	
+	// Swagger   (http://localhost:9000/swagger-ui.html)
 	// https://springframework.guru/spring-boot-restful-api-documentation-with-swagger-2/
-//	https://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	//	https://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
+
+
+	// Deploy to tomcat  
+	//	https://www.vojtechruzicka.com/spring-boot-war/
+	//	https://www.baeldung.com/spring-boot-war-tomcat-deploy
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
